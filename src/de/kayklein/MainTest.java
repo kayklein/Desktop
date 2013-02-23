07df0005f35e6bfcaeeb915f348f20a3c3696c9c
kayklein <it-kay-klein@web.de>
2013-02-23 01:38:16 +0100

    Update junit test framework

--- src/de/kayklein/MainTest.java
+++ src/de/kayklein/MainTest.java
@@ -1,25 +1,28 @@
 package de.kayklein;
 
-import junit.framework.TestCase;
+import org.junit.Test;
 
 import static de.kayklein.Main.MSG;
+import static junit.framework.Assert.assertEquals;
 
 /**
  * Created with IntelliJ IDEA.
  * User: Al-Quaida
  * Date: 23.02.13
- * Time: 01:28
+ * Time: 01:34
  * To change this template use File | Settings | File Templates.
  */
-public class MainTest extends TestCase {
+public class MainTest {
 
 
+    @Test
     public void testGetValue() throws Exception {
         int result = new Main().getValue();
         int expected = 4711;
         assertEquals(expected, result);
     }
 
+    @Test
     public void testGetMessage() throws Exception {
         String result = new Main().getMessage();
         String expected = MSG;
